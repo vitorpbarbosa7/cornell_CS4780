d8694c04f245b97386e50c1ea9a1716be8be2ab2
vpbmac <vitorpbarbosa7@gmail.com>
2023-06-29 18:28:50 -0300

    dummy before recursion

--- 13_kdtree/try.py
+++ 13_kdtree/try.py
@@ -1,8 +1,12 @@
+# TODO - store the structure
+# TODO - store the divisor
+
 import numpy as np
 
 
-points = [1,1,2,3,4,5,6,7,8]
+points = [1,1,2,3,4,5,6,7,8,9,10,11,12]
 
+tree = []
 points = sorted(points)
 n = len(points)
 m = n // 2 
@@ -13,12 +17,20 @@ right = points[m:]
 print(left)
 print(right)
 
+# ----------------------
+tree.append([left,right])
+
 # continue
 m = len(left) // 2
-left = left[:m]
+left_left = left[:m]
+left_right = left[m:]
+
+print(left_left, left_right)
 
 m = len(right) // 2
-right =  right[m:]
+right_left = right[:m]
+right_right =  right[m:]
 
-print(left)
-print(right)
+print(right_left, right_right)
+
+tree.append([left,right])
