c4be320a8cc2d5bf084728aaceeeb2e2e3bc0ae9
vpbmac <vitorpbarbosa7@gmail.com>
2023-06-29 18:38:35 -0300

    kind of the class worked

--- 13_kdtree/try.py
+++ 13_kdtree/try.py
@@ -1,10 +1,27 @@
 # TODO - store the structure
 # TODO - store the divisor
 
-import numpy as np
-
-
 points = [1,1,2,3,4,5,6,7,8,9,10,11,12]
+
+class kdTree:
+
+    def __init__(self, points):
+
+        # if it has points, so continue to add nodes to the left and to the right
+        if len(points) > 1:
+
+            points = sorted(points)
+            m = len(points) // 2
+
+            print(points)
+            breakpoint()
+
+            self.left = kdTree(points[:m])
+            self.right = kdTree(points[m:])
+
+kdtree = kdTree(points)
+print(kdtree)
+exit()
 
 tree = []
 points = sorted(points)
