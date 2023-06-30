91f004f483dfd12f7fdfd1e5acb4efa1481f9158
vpbmac <vitorpbarbosa7@gmail.com>
2023-06-29 22:43:35 -0300

    nn with only search and not calculating distances

--- 13_kdtree/try.py
+++ 13_kdtree/try.py
@@ -1,11 +1,14 @@
-# TODO - store the structure
-# TODO - store the divisor
+# TODO - store the structure -- OK
+# TODO - store the divisor -- OK 
+# TODO - method to with new node, find the nearest neighbors
 
-points = [1,1,2,3,4,5,6,7,8,9,10,11,12]
+points = [1,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,18,20,23]
+
+points = [1,1,2,3,4,5,6,8]
 
 class kdTree:
 
-    def __init__(self, points):
+    def __init__(self, points, min_points_node:int = 2):
 
         # if it has points, so continue to add nodes to the left and to the right
         if len(points) > 1:            
@@ -13,7 +16,8 @@ class kdTree:
             points = sorted(points)
             m = len(points) // 2
 
-            self.divisor = m
+            # if its equal or greater than this one, it will go right
+            self.divisor = points[m]
 
             print(points)
             print(self.divisor)
@@ -22,10 +26,33 @@ class kdTree:
             self.left = kdTree(points[:m])
             self.right = kdTree(points[m:])
 
-        if len(points) == 1:
+        if len(points) == min_points_node:
+            
+            # create a object node_points only at the last level
+            # here we can be at right or left side, important it is only at last level
+            self.node_points = points
 
-            self.left = points
-            self.right = points
+    def new_point(self, npoint):
+
+        # stop criteria
+        # we're at the last level if there are points in it
+        if hasattr(self, 'node_points'):
+            print(f'Nearest neighbors are: {self.node_points}')
+            return self.node_points
+
+        if npoint >= self.divisor:
+            print(f'passou aqui com {npoint} e divisor {self.divisor}')
+            # go right
+
+            # the object to go further into the search will be the right node (which can also contain more nodes, and also contains the method new_point as it is from this class)
+            self = self.right
+            self.new_point(npoint)
+        else:
+            # same comment here, same thing
+            self = self.left
+            self.new_point(npoint)
+
+
 
 
 kdtree = kdTree(points)
